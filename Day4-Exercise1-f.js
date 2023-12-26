1f18af966cdcbd16aa3bf96e51cc458418a37697
Ajay Kumar <96405422+AjayKumar-DRB@users.noreply.github.com>
2023-12-26 17:12:16 +0530

    Update Day4-Exercise1-f.js

--- Day4-Exercise1-f.js
+++ Day4-Exercise1-f.js
@@ -13,6 +13,9 @@ inp.on("line", (data) => {
 });
 
 inp.on("close", () => {
+  //Input format:1 2 3 4 5 6
+  //             7 8 9 10 11 12
+  //First input array should be in line one and other array in line two
   var arr1 = userInput[0].split(' ').map(Number);
   var arr2 = userInput[1].split(' ').map(Number);
 
